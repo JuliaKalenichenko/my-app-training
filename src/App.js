b7b29ab25dcf8662636472ca5ca0c7b1fb54dca7
Julia <kalenichenkojulia@gmail.com>
2021-11-03 17:32:11 +0200

    Write React class Component. Props(state) -> function return new object state -> (Method setState -> state.years). Render onclick -> button -> +value.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component, StrictMode} from "react";
+import {Component} from "react";
 import './App.css';
 
 /* const Header = () => {
@@ -63,7 +63,7 @@ function App() {
 
 // Props - object
 
-function WhoAml ({name, surname, link}) {
+/* function WhoAml ({name, surname, link}) {
     return (
         <div>
             <h1>My name is {name.firstName}, surname - {surname}</h1>
@@ -71,16 +71,54 @@ function WhoAml ({name, surname, link}) {
         </div>
     )
 }
+ */
+
+// У Component может быть динамическое состоянии которое меняеться.
+// Меняються как у function and class
+// state - на прямую менять нельзя, только через method setState.
+// setState - асинхронная операция.
+// Если нужна точность то колбекфункция.
+// Мы можем менять только те состояния обьекта которые нам нужны
+// .. остальные остануться без изменений.
+
+class WhoAml extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            text: '+++'
+        }
+    }
+    // setState - вызывает перерисовку компонента
+    nextYear = () => {
+
+        // function return new object state
+        this.setState(state => ({
+          years: state.years + 1
+        }))
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}+</button>
+                <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+                <a href={link}>My profile</a>
+            </div>
+        )
+    }
+}
 
 function App() {
     return (
         <div className="App">
             <WhoAml name="John" surname="Smith" link="facebook.com"/>
-            <WhoAml name={{firstName: 'Alex'}} surname="Shepard" link="vk.com"/>
+            <WhoAml name="Alex" surname="Shepard" link="vk.com"/>
         </div>
     );
 }
 //  {name()}
 // <WhoAml name={() => {return 'Julia'}} surname="Kalenichenko" link="vk.com"/>
-
+// <WhoAml name={{firstName: 'Alex'}} surname="Shepard" link="vk.com"/>
 export default App;
